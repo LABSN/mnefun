7b0b8f787cd03eba8b53f851c2820fafc6390dda
Kambiz Tavabi <ktavabi@gmail.com>
2015-04-09 17:13:42 -0700

    FIX:mk-dense-head-bug-fix

--- bin/run_anatomy.py
+++ bin/run_anatomy.py
@@ -30,6 +30,7 @@ parser.add_argument('--skip_recon', action='store_true', help='Set to skip FS re
 parser.add_argument('--mp', default=8, help='Multi threading argument for recon-all. Default 8.', type=int)
 parser.add_argument('--ico', default=4, help='BEM icosahedral parameter for watershed surfaces. Default 4.',
                     type=int)
+parser.add_argument('--skip_bem', action='store_true', help='Set to skip BEM computation.')
 
 args = parser.parse_args()
 
@@ -37,6 +38,7 @@ args = parser.parse_args()
 subj = args.subject
 bem_type = args.layers
 skip_recon = args.skip_recon
+skip_bem = args.skip_bem
 openmp = args.mp
 bem_ico = args.ico
 subj_raw_mri_dir = op.join(FS_DIRECTORY, 'DICOM', subj)
@@ -98,50 +100,51 @@ if not skip_recon:
     check_call(recon.split())
 
 # Start BEM routines
-mne_setup_mri = 'mne_setup_mri mri T1 --subject %s --overwrite' % subj
-check_call(mne_setup_mri.split())
+if not skip_recon:
+    mne_setup_mri = 'mne_setup_mri mri T1 --subject %s --overwrite' % subj
+    check_call(mne_setup_mri.split())
 
-if bem_type == 3:
-    # MEEG BEM from LABSN script
-    print('Creating 3-layer BEM for %s ' %subj)
-    param_maps_dir = op.join(fs_mri_dir, 'flash/parameter_maps')
-    if not op.isdir(param_maps_dir):
-        os.makedirs(param_maps_dir)
-    os.chdir(param_maps_dir)
-    for fn in ('flash5', 'flash30'):
-        mri_convert = 'mri_convert %s/%s.nii %s.mgz' % (fs_nii_dir, fn, fn)
+    if bem_type == 3:
+        # MEEG BEM from LABSN script
+        print('Creating 3-layer BEM for %s ' %subj)
+        param_maps_dir = op.join(fs_mri_dir, 'flash/parameter_maps')
+        if not op.isdir(param_maps_dir):
+            os.makedirs(param_maps_dir)
+        os.chdir(param_maps_dir)
+        for fn in ('flash5', 'flash30'):
+            mri_convert = 'mri_convert %s/%s.nii %s.mgz' % (fs_nii_dir, fn, fn)
+            check_call(mri_convert.split())
+            fsl_rigid_register = 'fsl_rigid_register -r %s/rawavg.mgz -i %s.mgz -o %s_reg.mgz' % (fs_mri_dir, fn, fn)
+            check_call(fsl_rigid_register.split())
+            os.mkdir(op.join(fs_mri_dir, '%s' % fn))
+            mri_convert = 'mri_convert -ot cor %s/%s_reg.mgz %s/%s' % (param_maps_dir, fn, fs_mri_dir, fn)
+            check_call(mri_convert.split())
+        os.chdir(fs_mri_dir)
+        mri_convert = 'mri_convert -ot cor brainmask.mgz brain'
         check_call(mri_convert.split())
-        fsl_rigid_register = 'fsl_rigid_register -r %s/rawavg.mgz -i %s.mgz -o %s_reg.mgz' % (fs_mri_dir, fn, fn)
-        check_call(fsl_rigid_register.split())
-        os.mkdir(op.join(fs_mri_dir, '%s' % fn))
-        mri_convert = 'mri_convert -ot cor %s/%s_reg.mgz %s/%s' % (param_maps_dir, fn, fs_mri_dir, fn)
-        check_call(mri_convert.split())
-    os.chdir(fs_mri_dir)
-    mri_convert = 'mri_convert -ot cor brainmask.mgz brain'
-    check_call(mri_convert.split())
-    mri_convert = 'mri_convert -ot cor T1.mgz T1'
-    mri_make_bem_surfaces = 'mri_make_bem_surfaces %s' % subj
-    check_call(mri_make_bem_surfaces.split())
-    watershed_bem = 'mne_watershed_bem --subject %s --overwrite' % subj
-    check_call(watershed_bem.split())
-    os.chdir(subj_bem_dir)
-    for srf in ('inner', 'outer'):
-        mne_convert_surf = 'mne_convert_surface --tri %s_skull.tri --swap --surfout %s_skull.surf' % (srf, srf)
-        check_call(mne_convert_surf.split())
-    copy(op.join(subj_dir, 'bem/watershed/%s_outer_skin_surface' % subj),
-         op.join(subj_dir, 'bem/outer_skin.surf'))
-else:
-    print('Creating single layer BEM for %s ' %subj)
-    os.chdir(fs_mri_dir)
-    watershed_bem = 'mne_watershed_bem --subject %s --overwrite' % subj
-    check_call(watershed_bem.split())
-    os.symlink(op.join(fs_nii_dir, op.basename(input_rage[0])), op.join(fs_nii_dir, 'MEMPRAGE.nii'))
-    for srf in ('inner_skull', 'outer_skull', 'outer_skin'):
-        os.symlink(op.join(subj_dir, 'bem/watershed/%s_%s_surface' % (subj, srf)),
-                   op.join(subj_dir, 'bem/%s.surf' % srf))
+        mri_convert = 'mri_convert -ot cor T1.mgz T1'
+        mri_make_bem_surfaces = 'mri_make_bem_surfaces %s' % subj
+        check_call(mri_make_bem_surfaces.split())
+        watershed_bem = 'mne_watershed_bem --subject %s --overwrite' % subj
+        check_call(watershed_bem.split())
+        os.chdir(subj_bem_dir)
+        for srf in ('inner', 'outer'):
+            mne_convert_surf = 'mne_convert_surface --tri %s_skull.tri --swap --surfout %s_skull.surf' % (srf, srf)
+            check_call(mne_convert_surf.split())
+        copy(op.join(subj_dir, 'bem/watershed/%s_outer_skin_surface' % subj),
+             op.join(subj_dir, 'bem/outer_skin.surf'))
+    else:
+        print('Creating single layer BEM for %s ' %subj)
+        os.chdir(fs_mri_dir)
+        watershed_bem = 'mne_watershed_bem --subject %s --overwrite' % subj
+        check_call(watershed_bem.split())
+        os.symlink(op.join(fs_nii_dir, op.basename(input_rage[0])), op.join(fs_nii_dir, 'MEMPRAGE.nii'))
+        for srf in ('inner_skull', 'outer_skull', 'outer_skin'):
+            os.symlink(op.join(subj_dir, 'bem/watershed/%s_%s_surface' % (subj, srf)),
+                       op.join(subj_dir, 'bem/%s.surf' % srf))
 
-mne_setup_fwd = 'mne_setup_forward_model --surf --ico %.0f --subject %s' % (bem_ico, subj)
-check_call(mne_setup_fwd.split())
+    mne_setup_fwd = 'mne_setup_forward_model --surf --ico %.0f --subject %s' % (bem_ico, subj)
+    check_call(mne_setup_fwd.split())
 
 # Create dense head surface
 if not op.isfile(op.join(subj_dir, 'bem/%s-head-dense.fif' % subj)):
@@ -151,8 +154,8 @@ if not op.isfile(op.join(subj_dir, 'bem/%s-head-dense.fif' % subj)):
     os.chdir(subj_bem_dir)
     mne_surf2bem_ = 'mne_surf2bem --surf %s/surf/lh.seghead --id 4 --check --fif %s-head-dense.fif' % (subj_dir, subj)
     check_call(mne_surf2bem_.split())
-else:
-    os.remove(op.join(subj_bem_dir, '%s-head.fif' % subj))
+    if op.isfile(op.join(subj_bem_dir, '%s-head.fif' % subj)):
+        os.rename(op.join(subj_bem_dir, '%s-head.fif' % subj), op.join(subj_bem_dir, '%s-head-sparse.fif' % subj))
     os.symlink(op.join(subj_bem_dir, '%s-head-dense.fif' % subj),
                op.join(subj_bem_dir, '%s-head.fif' % subj))
 print('Done')
