60dbca4cfa4929f25aee3bd91f23d4939516a198
Kambiz Tavabi <ktavabi@gmail.com>
2015-08-19 13:37:26 -0700

    FIX:close psd plotting window to prevent memory consumption

--- mnefun/_mnefun.py
+++ mnefun/_mnefun.py
@@ -1375,6 +1375,9 @@ def do_preprocessing_combined(p, subjects):
                                exclude=[])
             assert p.auto_bad_flat is None or isinstance(p.auto_bad_flat, dict)
             assert p.auto_bad_reject is None or isinstance(p.auto_bad_reject, dict)
+            if p.auto_bad_reject is None and p.auto_bad_flat is None:
+                raise RuntimeError('Auto bad channel detection active. Noisy and flat channel detection'
+                                   'parameters not defined. At least one criteria must be defined.')
             epochs = Epochs(raw, events, None, p.tmin, p.tmax,
                             baseline=_get_baseline(p), picks=picks,
                             reject=p.auto_bad_reject, flat=p.auto_bad_flat,
