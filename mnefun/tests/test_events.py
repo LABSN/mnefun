4e0eafbcd99b77598029816c80a63027a2485862
Jasper <japsai@gmail.com>
2016-08-12 15:29:54 -0700

    Fixed existing tests; failing because of 'inspect' use and 'print_status'.

--- mnefun/tests/test_events.py
+++ mnefun/tests/test_events.py
@@ -8,22 +8,25 @@ def test_event():
     params = mnefun.Params()
     setdefaults(params)
     params.on_process = handler
-    with patch('mnefun._mnefun.save_epochs') as func:
-        mnefun.do_processing(params,
-            write_epochs=True,
-        )
-        handler.assert_called_with('Doing epoch EQ/DQ', func, func(), params)
+    with patch('mnefun._mnefun.inspect'):
+        with patch('mnefun._mnefun.save_epochs') as func:
+            mnefun.do_processing(params,
+                write_epochs=True, 
+                print_status=False
+            )
+            handler.assert_called_with('Doing epoch EQ/DQ', func, func(), params)
 
 
 def setdefaults(params):
     params.score = Mock()
     params.subject_indices = []
-    params.subjects = []
-    params.structurals = []
-    params.dates = []
+    params.subjects = [None]
+    params.structurals = [None]
+    params.dates = [None]
     params.in_names = []
     params.in_numbers = []
     params.analyses = []
     params.out_names = []
     params.out_numbers = []
     params.must_match = []
+    params.decim = 5
