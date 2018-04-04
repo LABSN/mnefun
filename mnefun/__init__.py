51ae8fe75fd6ddba8339ad9886b920e4a2f42049
Eric Larson <larson.eric.d@gmail.com>
2018-04-04 16:29:55 -0400

    FIX: Fix import

--- mnefun/__init__.py
+++ mnefun/__init__.py
@@ -7,6 +7,6 @@ from ._mnefun import (
     info_sss_basis, plot_reconstruction, plot_chpi_snr_raw,
     get_fsaverage_label_operator, compute_good_coils, plot_good_coils,
     compute_auc, combine_medial_labels, clean_brain, plot_colorbar)
-from stats import anova_time
+from .stats import anova_time
 
 __version__ = '0.1.0.dev0'
